bcf15aa837c2afd736ec347a146d472e0426e069
Adekunle Seun <spydee4real@gmail.com>
2016-10-27 06:13:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+(function (window) {
 // ******************************* 
 // START HERE IF YOU WANT AN EASIER STARTING POINT FOR THIS ASSIGNMENT
 // *******************************
@@ -33,7 +34,6 @@ WARNING!!! WARNING!!!
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
-(function (window) {
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
